ac3200fd7fe902b0f25c1339289fc22d28b22f01
sy-app <seijuslp4@icloud.com>
2021-11-21 06:46:08 +0900

    ツイートを投稿するアカウントをmodeによって分ける

--- app/main.py
+++ app/main.py
@@ -6,7 +6,8 @@ from post_tweet import *
 def main(event, context):
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = event['Records'][0]['s3']['object']['key']
+    mode = context.invoked_function_arn.split(':')[-1]
 
     set_post_image(bucket, key)
-    twitter = set_twitter()
+    twitter = set_twitter(mode)
     post_tweet(twitter)
