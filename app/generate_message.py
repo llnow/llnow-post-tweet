d27d128956c53ed05a5844888f62c16dbf2a842d
sy-app <seijuslp4@icloud.com>
2021-12-07 17:40:31 +0900

    ツイートの検索キーワードなどをLambda間で共有するための改修
    * 生誕祭ハッシュタグをキーワードと別で取得しない
    
    * ツイート文に書くキーワードを実行時刻をもとにDynamoDbから取得するのではなくtweets_features>search_metadata>queryから取得する

--- app/generate_message.py
+++ app/generate_message.py
@@ -1,38 +1,14 @@
-import boto3
-from boto3.dynamodb.conditions import Key
-from check_datetime_in_range import *
-from generate_bd_hashtag import *
+from read_tweets_features import *
+from get_keywords import *
 
 
-def generate_message(mode):
-    table = boto3.resource('dynamodb').Table('ll-now-search-keyword')
+def generate_message(bucket):
+    tweets_features = read_tweets_features(bucket)
 
     # 検索キーワードを取得
-    # default_keywordsを取得
-    res = table.query(
-        KeyConditionExpression=Key('type').eq('default')
-    )
-    default_keywords = [d['keyword'] for d in res['Items']]
-
-    # option_keywordsを取得
-    res = table.query(
-        KeyConditionExpression=Key('type').eq('option')
-    )
-    option_keywords = []
-    option_list = res['Items']
-    for option in option_list:
-        kw = option['keyword']
-        since_str = option['since']
-        until_str = option['until']
-        if check_datetime_in_range(since_str, until_str):
-            option_keywords.append(kw)
-
-    keywords = default_keywords + option_keywords
+    keywords = get_keywords(tweets_features)
     kw_str = ' '.join(keywords)
 
-    # 生誕祭ハッシュタグを取得
-    bd_hashtag = generate_bd_hashtag(mode)
-
-    message = '現在の {} の様子です。\n\n{} #LLNow'.format(kw_str, bd_hashtag)
+    message = '現在の {} の様子です。\n\n#LLNow'.format(kw_str)
 
     return message
